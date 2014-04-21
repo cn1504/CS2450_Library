7a27e97b41304fd468e5d4b8861d62292d287c3c
shamblesides <nigel.mark.nelson@gmail.com>
2014-04-21 02:58:38 -0600

    Tests
    -Added test for the Library class
    -Updated sample database to include all media types

---
+++ UnitTestProject1/Library_Tests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CS2450_Library;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class Library_Tests
+    {
+        [TestMethod]
+        public void MakeEmptyLibrary()
+        {
+            //new library
+            Library lib = new Library();
+            //assure that it's empty
+            foreach (Item i in lib.GetCatalog())
+            {
+                Assert.Fail("Item in new Library");
+            }
+            foreach (Patron i in lib.GetPatrons())
+            {
+                Assert.Fail("Patron in new Library");
+            }
+            foreach (Item i in lib.GetOverdue(DateTime.Now))
+            {
+                Assert.Fail("Overdue Item in empty Library");
+            }
+        }
+    }
+}
